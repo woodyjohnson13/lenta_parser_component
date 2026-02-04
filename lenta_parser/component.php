aced98f11a39474f3ad79400d36282cc60efe79b
Wooody <johnsonwoody13@gmail.com>
2026-02-04 17:18:56 +0300

    feat: add gobal variables of blocks id and parse response

--- lenta_parser/component.php
+++ lenta_parser/component.php
@@ -3,4 +3,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 class LentaParserComponent extends CBitrixComponent
 {
+    private $iblock_id = null;
+    private $highblock_id = null;
+    private $parse_response = null;
 }
