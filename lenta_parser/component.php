2b881b0a4e31eccb0ebdd9a2bc3be089f880b824
Wooody <johnsonwoody13@gmail.com>
2026-02-04 17:56:11 +0300

    feat: add execute_component method, check and init blocks methods

--- lenta_parser/component.php
+++ lenta_parser/component.php
@@ -6,4 +6,48 @@ class LentaParserComponent extends CBitrixComponent
     private $iblock_id = null;
     private $highblock_id = null;
     private $parse_response = null;
+    public function executeComponent()
+    {
+        try {
+            $this->check_modules();
+            $this->init_blocks();
+        } catch (Exception $e) {
+            ShowError($e->getMessage());
+        }
+    }
+    
+    private function check_modules()
+    {
+        if (!CModule::IncludeModule("iblock")) {
+            throw new Exception("Модуль инфоблоков не установлен");
+        }
+        if (!CModule::IncludeModule("highloadblock")) {
+            throw new Exception("Модуль highload блоков не установлен");
+        }
+    }
+    
+    private function init_blocks()
+    {
+        $res = CIBlock::GetList([], [
+            'CODE' => 'news',
+            'TYPE' => 'content',
+            'ACTIVE' => 'Y'
+        ]);
+
+        if ($iblock = $res->Fetch()) {
+            $this->iblock_id = $iblock['ID'];
+        } else {
+            throw new Exception("Инфоблок не найден");
+        }
+        
+        $highload_block = Bitrix\Highloadblock\HighloadBlockTable::getList([
+            'filter' => ['=NAME' => 'Categories']
+        ])->fetch();
+        
+        if ($highload_block) {
+            $this->highblock_id = $highload_block['ID'];
+        } else {
+            throw new Exception("Highload блок не найден");
+        }
+    }
 }
