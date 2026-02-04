440d910a92c7f8f24179d0af6be468800fa2fe79
Wooody <johnsonwoody13@gmail.com>
2026-02-04 22:07:12 +0300

    feat: add header and style

--- lenta_parser/templates/default/template.php
+++ lenta_parser/templates/default/template.php
@@ -5,4 +5,17 @@ $this->addExternalCss($this->GetFolder() . '/style.css');
 ?>
 
 <div class="lenta-parser">
+    <div class="lenta-header">
+        <h1>Парсер новостей "Ленты"</h1>
+        <div class="subtitle">Самые свежие новости в реальном времени</div>
+    </div>
+
+    <div class="actions-container">
+        <form method="post" id="parse-form">
+            <input type="hidden" name="action" value="parse">
+            <button type="submit" class="action-button success" id="parse-button">
+                <span>🔄</span> Загрузить свежие новости
+            </button>
+        </form>
+    </div>
 </div>
