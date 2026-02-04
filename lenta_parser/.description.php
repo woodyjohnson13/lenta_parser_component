fb5561c5a47966c5ef403a9158c31322015c4203
Wooody <johnsonwoody13@gmail.com>
2026-02-05 00:56:13 +0300

    feat: add description and class files

---
+++ lenta_parser/.description.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$component_description = [
+    "NAME" => "Парсер новостей Ленты",
+    "DESCRIPTION" => "Компонент для парсинга и отображения новостей с сайта Ленты",
+    "PATH" => [
+        "ID" => "custom",
+        "NAME" => "Кастомные компоненты"
+    ],
+    "CACHE_PATH" => "Y",
+    "COMPLEX" => "N"
+];
---
+++ lenta_parser/class.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+include_once(__DIR__.'/component.php');
+?>
